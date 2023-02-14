12b846c90bcae18ffa869cb46b248dc767ea1beb
Yash Mangal <104120843+yashmangal112@users.noreply.github.com>
2023-02-14 21:46:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
+import "./App.css";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
+import Event from "./pages/Event.js";
+import Team from "./pages/Team.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/event" element={<Event />} />
+          <Route exact path="/team" element={<Team />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
