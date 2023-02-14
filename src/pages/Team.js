c98eff97198be578eb549c9d4befca8d3552815d
Yash Mangal <104120843+yashmangal112@users.noreply.github.com>
2023-02-14 21:45:11 +0530

    made event, team page

---
+++ src/pages/Team.js
@@ -0,0 +1,30 @@
+import "../Team.css";
+import TeamCard from "../pages/Team-card.js";
+import Header from "../common/Header.js";
+import Footer from "../common/Footer.js";
+import image from "../images/demo-team.jpg";
+function Team(){
+    return (
+        <div>
+            <Header/>
+            <div className="bg-image-team"></div>
+            <div className="heading"><p>OUR TEAM</p></div>
+            <div className="container">
+            <div className="flex-container">
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+                <TeamCard image={image} name="Yash Mangal" post="Sports Secretary" />
+            </div>
+            </div>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Team;
