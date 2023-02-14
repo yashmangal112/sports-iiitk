a9485117aba35760281df2ac1d749dee7838a607
RujulP14 <prujul14@gmail.com>
2023-02-14 12:31:50 +0530

    Made Contact and About Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Home from "./components/Home.js";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Contact/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
