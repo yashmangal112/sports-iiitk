a242019fd88edbc1a1feb6465a2420d50b4a1566
Shreyansh <jainshreyansh233@gmail.com>
2023-02-15 22:01:36 +0530

    Gallery page and active navigation bar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import "./App.css";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
+import Gallery from "./pages/Gallery_mainpg";
 import Event from "./pages/Event.js";
 import Team from "./pages/Team.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/event" element={<Event />} />
-          <Route exact path="/team" element={<Team />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/gallery" element={<Gallery />} />
         </Routes>
       </Router>
     </div>
--- src/common/Header.js
+++ src/common/Header.js
@@ -1,21 +1,40 @@
-function Header(){
-    return (
-        <div>
-            <div className="header">
-                <div className="list">
-                    <ul>
-                        <li className="headlist"><a href="/" className="button">HOME</a></li>
-                        <li className="headlist"><a href="/about" className="button">ABOUT</a></li>
-                        <li className="headlist"><a href="/team" className="button">TEAM</a></li>
-                        <li className="headlist"><a href="event" className="button">EVENT</a></li>
-                        <li className="headlist"><a href="contact" className="button">CONTACT</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="logo">
-                SPORTS
-            </div>
+import { NavLink } from "react-router-dom";
+function Header() {
+  return (
+    <div>
+      <div className="header">
+        <div className="list">
+          <ul>
+            <li className="headlist">
+              <NavLink to="/" className="button">
+                HOME
+              </NavLink>
+            </li>
+            <li className="headlist">
+              <NavLink to="/about" className="button">
+                ABOUT
+              </NavLink>
+            </li>
+            <li className="headlist">
+              <NavLink to="/team" className="button">
+                TEAM
+              </NavLink>
+            </li>
+            <li className="headlist">
+              <NavLink to="/event" className="button">
+                EVENT
+              </NavLink>
+            </li>
+            <li className="headlist">
+              <NavLink to="/contact" className="button">
+                CONTACT
+              </NavLink>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+      <div className="logo">SPORTS</div>
+    </div>
+  );
 }
 export default Header;
