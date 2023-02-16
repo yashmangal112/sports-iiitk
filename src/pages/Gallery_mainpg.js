a0184251ad29762b18ac5f8f22a722312c786de2
Shreyansh <jainshreyansh233@gmail.com>
2023-02-17 00:12:24 +0530

    Gallery Page Done

--- src/pages/Gallery_mainpg.js
+++ src/pages/Gallery_mainpg.js
@@ -1,4 +1,6 @@
 import "../Gallery_mp.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import Header from "../common/Header.js";
 import Footer from "../common/Footer.js";
 function Gallery() {
@@ -8,11 +10,42 @@ function Gallery() {
       <div>
         <div className="gallery-bg"></div>
         <div className="heading-gallery">
-                <p className="heading1">View Gallery</p>
-                <img className="view-bg" src={require("../images/View-Gallery.png")} alt="#"></img>
-                <p className="content-gallery">Taking part in sports is important as it reduces stress and enhances mood. It builds healthy bones and muscles, increases fitness, improves sleep, helps people socialize, improves cooperation skills and team spirit, boosts self confidence and lowers the risk of getting obese.</p>
-            </div>
+          <p className="heading1">View Gallery</p>
+          <img
+            className="view-bg"
+            src={require("../images/View-Gallery.png")}
+            alt="#"
+          ></img>
+          <p className="content-gallery">
+            Taking part in sports is important as it reduces stress and enhances
+            mood. It builds healthy bones and muscles, increases fitness,
+            improves sleep, helps people socialize, improves cooperation skills
+            and team spirit, boosts self confidence and lowers the risk of
+            getting obese.
+          </p>
+        </div>
+        <div className="rectangle-left"></div>
+        <div className="rectangle-right"></div>
+        <Carousel infiniteLoop autoPlay>
+          <div className="image">
+             <img src={require("../images/Rectangle 8.png")} alt="#" />
+          </div>
+          <div className="image">
+             <img src={require("../images/Rectangle 10.png")} alt="#" />
+          </div>
+          <div className="image">
+             <img src={require("../images/Rectangle 17.png")} alt="#" />
+          </div>
+          <div className="image">
+             <img src={require("../images/Rectangle 11.png")} alt="#" />
+          </div>
+          <div className="image">
+             <img src={require("../images/Rectangle 17.png")} alt="#" />
+          </div>
+          
+        </Carousel>
       </div>
+      <Footer/>
     </div>
   );
 }
